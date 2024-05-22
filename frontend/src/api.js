c394b2201769696ff7ee999e34baef7b626b27a6
RaufunNazin <raufun.nazin13@gmail.com>
2024-05-23 00:41:34 +0600

    port

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `https://snapgenix.com`,
+  baseURL: `https://snapgenix.com:8000`,
   timeout: 30000,
   withCredentials: true,
   headers: {
