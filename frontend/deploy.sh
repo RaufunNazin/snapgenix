b4218a00c80ce8ffde8bfcee5e5d4922a738e836
RaufunNazin <raufun.nazin13@gmail.com>
2024-05-22 23:59:59 +0600

    deploy.sh

---
+++ frontend/deploy.sh
@@ -0,0 +1,10 @@
+echo "switching to main"
+git checkout main
+
+echo "building frontend"
+npm run build
+
+echo "deploying frontend"
+scp -r dist/* root@93.127.206.98:/var/www/snapgenix/
+
+echo "done"
